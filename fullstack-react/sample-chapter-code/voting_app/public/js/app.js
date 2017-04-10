efc263a17e9ec15dbbee909c8391f6bd6f6cf2e7
cantastemel <cantastemel@gmail.com>
2017-04-10 09:06:45 -0700

    completing the preview book

--- fullstack-react/sample-chapter-code/voting_app/public/js/app.js
+++ fullstack-react/sample-chapter-code/voting_app/public/js/app.js
@@ -1,8 +1,46 @@
-class ProductList extends React.Component{
-	render(){
-		// Creating array of products from the Seed.product to return later
-		const productComponents = Seed.products.map((product) => (
-			<Product 
+class ProductList extends React.Component {
+
+    constructor(props) {
+        super(props); // always call this first
+
+        this.state = {
+            products: []
+        };
+
+        // Cutom method bindings here
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
+
+    handleProductUpVote(productId) {
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productId) {
+                // Create a new empty object and with the same basing of the current `product` object
+                // and modify its `votes` property by adding +1
+                return Object.assign({}, product, {
+                    votes: product.votes + 1,
+                });
+            } else {
+                return product;
+            }
+        });
+        this.setState({
+            products: nextProducts,
+        });
+    }
+
+    render() {
+        // Sort the producst based on the vote value and store them in products variable
+        const products = this.state.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+
+        // Create and store an array of products in productComponents and map the values in the component
+        const productComponents = products.map((product) => (
+            <Product 
 				key={'product-' + product.id}
 				id = {product.id}
 				title = {product.title}
@@ -11,27 +49,43 @@ class ProductList extends React.Component{
 				votes = {product.votes}
 				submitterAvatarUrl = {product.submitterAvatarUrl}
 				productImageUrl = {product.productImageUrl}
+				onVote = {this.handleProductUpVote}
 			/>
-		));
+        ));
 
-		return(
-			<div className='ui unstackable items'>
+        return (
+            <div className='ui unstackable items'>
 				{productComponents}
 			</div>
-		);
-	}
+        );
+    }
 }
 
-class Product extends React.Component{
-	render(){
-		return(
-			<div className='item'>
+class Product extends React.Component {
+    // Inside `render()` React will bind the `this` variable to the component automatically.
+    // We want `this` inside `handleUpVote()` to reference the component too, just like it does inside render().
+    // So, any time we define our own custom component methods (eg. `handleUpVote`), 
+    // we have to manually bind this to the component ourselves in the `constructor()`
+    constructor(props) {
+        super(props); // always call this first
+
+        // custom method bindings here
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id);
+    }
+
+    render() {
+        return (
+            <div className='item'>
 				<div className='image'>
 					<img src={this.props.productImageUrl} />
 				</div>
 				<div className='middle aligned content'>
 					<div className='header'>
-						<a><i className='large caret up icon' /></a> {this.props.votes}
+						<a onClick={this.handleUpVote} ><i className='large caret up icon' /></a> {this.props.votes}
 					</div>
 					<div className='description'>
 						<a href={this.props.url} >{this.props.title}</a>
@@ -43,11 +97,11 @@ class Product extends React.Component{
 					</div>
 				</div>
 			</div>
-		);
-	}
+        );
+    }
 }
 
-ReactDOM.render( 
-	<ProductList />, 
-	document.getElementById('content') 
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
 );
